3451bb9ea5d189a9c2010d851e0ea225e5191d8d
Your Name <you@example.com>
2024-09-22 23:56:19 +0600

    test

--- __tests__/plain.test.js
+++ __tests__/plain.test.js
@@ -3,17 +3,16 @@ import plain from '../src/formatters/plain.js';
 describe('Plain formatter', () => {
   test('should format added property', () => {
     const diff = [{ key: 'common.follow', action: 'added', value: false }];
-    expect(plain(diff)).toBe('Property "common.follow" was added with value: false');
+    expect(plain(diff)).toBe(`Property 'common.follow' was added with value: false`);
   });
 
   test('should format removed property', () => {
     const diff = [{ key: 'common.setting2', action: 'removed' }];
-    expect(plain(diff)).toBe('Property "common.setting2" was removed');
+    expect(plain(diff)).toBe(`Property 'common.setting2' was removed`);
   });
 
   test('should format updated property', () => {
     const diff = [{ key: 'common.setting3', action: 'updated', value: { old: true, new: null } }];
-    expect(plain(diff)).toBe('Property "common.setting3" was updated. From true to null');
+    expect(plain(diff)).toBe(`Property 'common.setting3' was updated. From true to null`);
   });
-
 });
