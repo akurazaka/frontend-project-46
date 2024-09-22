55e957193da660ca378574b746cc143323077b43
Your Name <you@example.com>
2024-09-22 23:52:40 +0600

    test

--- eslint.config.js
+++ eslint.config.js
@@ -1,52 +1,13 @@
-import globals from 'globals';
-
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-import { composeAbsFilepath } from './src/utils.js';
-
-const __dirname = composeAbsFilepath('.');
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
+import js from '@eslint/js';
 
 export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: { import: importPlugin },
-    rules: {
-      ...importPlugin.configs.recommended.rules,
-    },
-  },
-  ...compat.extends('airbnb-base'),
+  js.configs.recommended,
+
   {
     rules: {
-      'no-underscore-dangle': [
-        'error',
-        {
-          allow: ['__filename', '__dirname'],
-        },
-      ],
-      'import/extensions': [
-        'error',
-        {
-          js: 'always',
-        },
-      ],
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'no-console': 'off',
-      'import/no-extraneous-dependencies': 'off',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      quotes: ['error', 'single'],
     },
   },
 ];
