5ad379dffd1b6eaee715a3921b352ba915fab1b6
Your Name <you@example.com>
2024-09-22 23:23:36 +0600

    fix

--- Makefile
+++ Makefile
@@ -2,7 +2,5 @@ install:
 	npm ci
 gendiff:
 	node bin/gendiff.js
-lint:
-	npm lint
 test:
 	npm test
--- src/parsers.js
+++ src/parsers.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import * as fs from 'node:fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
