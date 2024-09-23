b33537c1cfbb8dbb468c143abbfc7a2100be8afe
Your Name <you@example.com>
2024-09-23 23:50:02 +0600

    test

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,63 +1,52 @@
 import _ from 'lodash';
 
-const formatKeyName = (key, changeType, element) => {
-  if ((changeType === 'added') || ((changeType === 'changed') && (Object.hasOwn(element, 'oldValue')))) {
-    return `  + ${key}`;
-  } 
-  if (changeType === 'deleted') {
-    return `  - ${key}`;
+const markers = {
+  added: '+',
+  removed: '-',
+  unchanged: ' ',
+  nested: ' ',
+};
+
+const createIndent = (level) => {
+  const space = ' ';
+  return space.repeat(level * 4 - 2);
+};
+
+const convertToString = (data, level = 1) => {
+  if (!_.isObject(data)) {
+    return data;
   }
-  return `    ${key}`;
+  const propertyNames = Object.keys(data);
+  const formattedProperties = propertyNames.map(
+    (property) => `${createIndent(level + 1)}  ${property}: ${convertToString(
+      data[property],
+      level + 1,
+    )}`,
+  );
+  return `{\n${formattedProperties.join('\n')}\n  ${createIndent(level)}}`;
 };
 
-const transformDiffArrayToObject = (diffArray) => {
-  return diffArray.reduce((accumulator, element) => {
-    const formattedKey = formatKeyName(element.name, element.diff, element);
-    const updatedAccumulator = accumulator;
-    if (Object.hasOwn(element, 'oldValue')) {
-      const oldKey = `  - ${element.name}`;
-      if (Array.isArray(element.value)) {
-        return {
-          ...updatedAccumulator, 
-          [oldKey]: element.oldValue, 
-          [formattedKey]: transformDiffArrayToObject(element.value),
-        };
-      };
-      return {
-        ...updatedAccumulator, 
-        [oldKey]: element.oldValue, 
-        [formattedKey]: element.value,
-      };
-    } 
-    if (Array.isArray(element.value)) {
-      return {
-        ...updatedAccumulator, 
-        [formattedKey]: transformDiffArrayToObject(element.value),
-      };
-    };
-    return {
-      ...updatedAccumulator, 
-      [formattedKey]: element.value,
-    };
-  }, {});
+const formatStylishly = (data, level = 1) => {
+  switch (data.type) {
+    case 'added':
+    case 'deleted':
+    case 'unchanged':
+      return `${createIndent(level)}${markers[data.type]} ${
+        data.key
+      }: ${convertToString(data.value, level)}`;
+    case 'changed':
+      return `${createIndent(level)}${markers.removed} ${
+        data.key
+      }: ${convertToString(data.valueBefore, level)}\n${createIndent(level)}${
+        markers.added
+      } ${data.key}: ${convertToString(data.valueAfter, level)}`;
+    case 'nested':
+      return `${createIndent(level)}  ${data.key}: {\n${data.children
+        .map((child) => formatStylishly(child, level + 1))
+        .join('\n')}\n ${createIndent(level)} }`;
+    default:
+      throw new Error(`Unknown type: ${data.type}`);
+  }
 };
 
-const formatObjectToString = (objectDiff, indent = '    ') => {
-  const recursiveStringify = (data, level) => {
-    if (!_.isObject(data)) {
-      return `${data}`;
-    };
-    const entries = Object.entries(data);
-    const resultString = entries.reduce((acc, [key, value]) => {
-      const currentIndent = (key.startsWith(' ')) ? (indent.repeat(level - 1)) : indent.repeat(level);
-      const newAccumulator = `${acc}${currentIndent}${key}: ${recursiveStringify(value, level + 1)}\n`;
-      return newAccumulator;
-    }, '');
-    return `{\n${resultString}${indent.repeat(level - 1)}}`;
-  };
-  return recursiveStringify(objectDiff, 1);
-};
-
-const stylishDiffFormatter = (diffArray) => formatObjectToString(transformDiffArrayToObject(diffArray));
-
-export default stylishDiffFormatter;
+export default (changes) => `{\n${changes.map((change) => formatStylishly(change, 1)).join('\n')}\n}`;
