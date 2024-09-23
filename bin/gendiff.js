eaeb15e80e4a63bb5151e801433b991eb3939753
Your Name <you@example.com>
2024-09-23 20:28:48 +0600

    test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import genDiff from '../src/index.js';
+import generateDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -11,5 +11,5 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .action((file1, file2) => console.log(genDiff(file1, file2, program.opts().format)))
+  .action((file1, file2) => console.log(generateDiff(file1, file2, program.opts().format)))
   .parse();
