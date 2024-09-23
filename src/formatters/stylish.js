009345922e865e938abb8d7f3dd4124563067d90
Your Name <you@example.com>
2024-09-24 00:25:53 +0600

    safe

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,49 +1,46 @@
-import _ from 'lodash';
-
-const replacer = ' ';
-const spaceCount = 4;
-
-const getIndentSize = (depth) => depth * spaceCount;
-const getIndent = (depth) => replacer.repeat(getIndentSize(depth) - 2);
-const getIndentBracket = (depth) => replacer.repeat(getIndentSize(depth) - spaceCount);
-
-const createString = (type, key, value, indent) => `${indent}${type}${key}: ${value}`;
-
-const stringify = (data, count) => {
-  if (!_.isObject(data)) { return data; }
-
-  const indent = getIndent(count);
-  const indentBracket = getIndentBracket(count);
-  const result = Object.keys(data).map((key) => createString('  ', key, stringify(data[key], count + 1), indent));
-
-  return ['{', ...result, `${indentBracket}}`].join('\n');
+const makeString = (obj, depth) => {
+  const iter = (currentObj, currentDepth) => {
+    const spacesCount = 1;
+    const replacer = ' ';
+    const indentSize = currentDepth * spacesCount;
+    const bracketIndent = replacer.repeat(indentSize - 4);
+    const currentIndent = replacer.repeat(indentSize);
+    if (typeof currentObj === 'object' && currentObj !== null) {
+      const line = Object.entries(currentObj)
+        .map(([key, value]) => `\n${currentIndent}${key}: ${iter(value, currentDepth + 4)}`).join('');
+      return `{${line}\n${bracketIndent}}`;
+    }
+    return currentObj;
+  }
+  const result = iter(obj, depth + 4);
+  return result;
 };
 
-const stylish = (tree) => {
-  const iter = (currentValue, count) => {
-    const indent = getIndent(count);
-    const indentBracket = getIndentBracket(count);
-    const result = currentValue.map((item) => {
-      switch (item.type) {
-        case 'added':
-          return createString('+ ', item.key, stringify(item.value, count + 1), indent);
-        case 'deleted':
-          return createString('- ', item.key, stringify(item.value, count + 1), indent);
-        case 'changed':
-          return `${createString('- ', item.key, stringify(item.value.old, count + 1), indent)}\n${createString('+ ', item.key, stringify(item.value.new, count + 1), indent)}`;
-        case 'node':
-          return createString('  ', item.key, iter(item.children, count + 1), indent);
-        case 'unchanged':
-          return createString('  ', item.key, stringify(item.value, count + 1), indent);
-        default:
-          throw new Error(`Error type - ${item.type}`);
+const stylish = (objects) => {
+  const iter = (currentValue, depth) => {
+    const spacesCount = 4;
+    const replacer = ' ';
+    const indentSize = depth * spacesCount;
+    const cutIndentSize = depth * spacesCount - 2;
+    const currentIndent = replacer.repeat(indentSize);
+    const cutCurrentIndent = replacer.repeat(cutIndentSize);
+
+    if (!currentValue.children) {
+      if (currentValue.type === 'added') {
+        return `${cutCurrentIndent}+ ${currentValue.name}: ${makeString(currentValue.value, indentSize)}\n`;
+      } if (currentValue.type === 'unchanged') {
+        return `${cutCurrentIndent}  ${currentValue.name}: ${makeString(currentValue.value, indentSize)}\n`;
+      } if (currentValue.type === 'removed') {
+        return `${cutCurrentIndent}- ${currentValue.name}: ${makeString(currentValue.value, indentSize)}\n`;
+      } if (currentValue.type === 'updated') {
+        return `${cutCurrentIndent}- ${currentValue.name}: ${makeString(currentValue.then, indentSize)}\n${cutCurrentIndent}+ ${currentValue.name}: ${makeString(currentValue.now, indentSize)}\n`;
       }
-    });
-
-    return ['{', ...result, `${indentBracket}}`].join('\n');
+    }
+    const nextObjects = currentValue.children.map((child) => iter(child, depth + 1)).join('');
+    return [`${currentIndent}${currentValue.name}: {\n${nextObjects}`, `${currentIndent}}\n`].join('');
   };
-
-  return iter(tree, 1);
+  const result = objects.map((object) => iter(object, 1)).join('');
+  return `{\n${result}}`;
 };
 
 export default stylish;
