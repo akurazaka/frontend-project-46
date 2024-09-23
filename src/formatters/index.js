8c1bf6f5b48bb3cbbd1b2b94b8edae48648c809a
Your Name <you@example.com>
2024-09-24 00:33:05 +0600

    safe

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,13 @@
-import stylish from './stylish.js';
 import plain from './plain.js';
+import stylish from './stylish.js';
+import json from './json.js';
 
-const formats = {
-  stylish,
-  plain,
-  json: (data) => JSON.stringify(data, null, 2),
+const chooseFormat = (data, format) => {
+  switch (format) {
+    case 'plain': return plain(data);
+    case 'json': return json(data);
+    default: return stylish(data);
+  }
 };
 
-export default (data, format) => formats[format](data);
+export default chooseFormat;
