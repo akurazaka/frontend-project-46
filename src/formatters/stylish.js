1adb6392fc82b859e908732314760503335e13c3
Your Name <you@example.com>
2024-09-23 23:53:42 +0600

    test

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,54 +1,55 @@
 import _ from 'lodash';
 
-const markers = {
+const statusMarkers = {
   matched: ' ',
   expected: '-',
   received: '+',
 };
-const getMarker = (stat) => {
-  if (!(_.has(markers, stat))) {
-    throw new Error(`Unknown stat "${stat}"`);
+
+const getStatusMarker = (status) => {
+  if (!(_.has(statusMarkers, status))) {
+    throw new Error(`Unknown status "${status}"`);
   }
-  return markers[stat];
+  return statusMarkers[status];
 };
 
-const composeIndent = (depth, spacesCount = 4, offset = 2) => ' '.repeat((depth * spacesCount) - offset);
-const stringify = (item, depth) => {
-  if (!(_.isPlainObject(item))) {
-    return `${item}`;
+const createIndent = (level, spaceCount = 4, offset = 2) => ' '.repeat((level * spaceCount) - offset);
+const convertToString = (element, level) => {
+  if (!(_.isPlainObject(element))) {
+    return `${element}`;
   }
-  const indentLines = composeIndent(depth);
-  const items = Object
-    .entries(item)
-    .map(([key, value]) => `${indentLines}${getMarker('matched')} ${key}: ${stringify(value, depth + 1)}`);
+  const indentLines = createIndent(level);
+  const entries = Object
+    .entries(element)
+    .map(([property, value]) => `${indentLines}${getStatusMarker('matched')} ${property}: ${convertToString(value, level + 1)}`);
 
-  const indentBrackets = composeIndent(depth, 4, 4);
-  return `{\n${items.join('\n')}\n${indentBrackets}}`;
+  const indentBrackets = createIndent(level, 4, 4);
+  return `{\n${entries.join('\n')}\n${indentBrackets}}`;
 };
 
-export default (tree) => {
-  const iter = (currentNode, depth) => {
-    const indentLines = composeIndent(depth);
-    const items = currentNode
+export default (dataTree) => {
+  const traverse = (currentNode, level) => {
+    const indentLines = createIndent(level);
+    const entries = currentNode
       .map((node) => {
-        const { stat, key } = node;
-        if (stat === 'exchanged') {
+        const { status, property } = node;
+        if (status === 'exchanged') {
           const { values } = node;
-          const { from: valueFrom, to: valueTo } = values;
+          const { from: oldValue, to: newValue } = values;
           return [
-            `${indentLines}${getMarker('expected')} ${key}: ${stringify(valueFrom, depth + 1)}`,
-            `${indentLines}${getMarker('received')} ${key}: ${stringify(valueTo, depth + 1)}`,
+            `${indentLines}${getStatusMarker('expected')} ${property}: ${convertToString(oldValue, level + 1)}`,
+            `${indentLines}${getStatusMarker('received')} ${property}: ${convertToString(newValue, level + 1)}`,
           ].join('\n');
         }
-        if (stat === 'nested') {
+        if (status === 'nested') {
           const { children } = node;
-          return `${indentLines}${getMarker('matched')} ${key}: ${iter(children, depth + 1)}`;
+          return `${indentLines}${getStatusMarker('matched')} ${property}: ${traverse(children, level + 1)}`;
         }
         const { value } = node;
-        return `${indentLines}${getMarker(stat)} ${key}: ${stringify(value, depth + 1)}`;
+        return `${indentLines}${getStatusMarker(status)} ${property}: ${convertToString(value, level + 1)}`;
       });
-    const indentBrackets = composeIndent(depth, 4, 4);
-    return `{\n${items.join('\n')}\n${indentBrackets}}`;
+    const indentBrackets = createIndent(level, 4, 4);
+    return `{\n${entries.join('\n')}\n${indentBrackets}}`;
   };
-  return iter(tree, 1);
+  return traverse(dataTree, 1);
 };
