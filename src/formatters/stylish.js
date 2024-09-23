7a39c6ac250158613cca8cb5111a4f4e3adeab13
Your Name <you@example.com>
2024-09-23 23:59:28 +0600

    safeProperty

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -30,24 +30,25 @@ const convertToString = (element, level) => {
 export default (dataTree) => {
   const traverse = (currentNode, level) => {
     const indentLines = createIndent(level);
-    const entries = currentNode
-      .map((node) => {
-        const { status, property } = node;
-        if (status === 'exchanged') {
-          const { values } = node;
-          const { from: oldValue, to: newValue } = values;
-          return [
-            `${indentLines}${getStatusMarker('expected')} ${property}: ${convertToString(oldValue, level + 1)}`,
-            `${indentLines}${getStatusMarker('received')} ${property}: ${convertToString(newValue, level + 1)}`,
-          ].join('\n');
-        }
-        if (status === 'nested') {
-          const { children } = node;
-          return `${indentLines}${getStatusMarker('matched')} ${property}: ${traverse(children, level + 1)}`;
-        }
-        const { value } = node;
-        return `${indentLines}${getStatusMarker(status)} ${property}: ${convertToString(value, level + 1)}`;
-      });
+    const entries = currentNode.map((node) => {
+      const { status, property } = node;
+      const safeProperty = property !== undefined ? property : 'undefined';
+
+      if (status === 'exchanged') {
+        const { values } = node;
+        const { from: oldValue, to: newValue } = values;
+        return [
+          `${indentLines}${getStatusMarker('expected')} ${safeProperty}: ${convertToString(oldValue, level + 1)}`,
+          `${indentLines}${getStatusMarker('received')} ${safeProperty}: ${convertToString(newValue, level + 1)}`,
+        ].join('\n');
+      }
+      if (status === 'nested') {
+        const { children } = node;
+        return `${indentLines}${getStatusMarker('matched')} ${safeProperty}: ${traverse(children, level + 1)}`;
+      }
+      const { value } = node;
+      return `${indentLines}${getStatusMarker(status)} ${safeProperty}: ${convertToString(value, level + 1)}`;
+    });
     const indentBrackets = createIndent(level, 4, 4);
     return `{\n${entries.join('\n')}\n${indentBrackets}}`;
   };
