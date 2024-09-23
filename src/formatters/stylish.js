50357cbf987814373a7f4617fa7c9c7b369747ec
Your Name <you@example.com>
2024-09-24 00:37:19 +0600

    safe

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,46 +1,50 @@
-const makeString = (obj, depth) => {
-  const iter = (currentObj, currentDepth) => {
-    const spacesCount = 4; // Убедитесь, что это значение совпадает с stylish
-    const replacer = ' ';
-    const indentSize = currentDepth * spacesCount;
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const currentIndent = replacer.repeat(indentSize);
-    if (typeof currentObj === 'object' && currentObj !== null) {
-      const line = Object.entries(currentObj)
-        .map(([key, value]) => `\n${currentIndent}${key}: ${iter(value, currentDepth + 1)}`).join('');
-      return `{${line}\n${bracketIndent}}`;
-    }
-    return currentObj;
-  }
-  const result = iter(obj, depth);
-  return result;
+import _ from 'lodash';
+
+const states = {
+  added: '+ ',
+  removed: '- ',
+  unchanged: '  ',
+  nested: '  ',
+  updated: ['- ', '+ '],
 };
 
-const stylish = (objects) => {
-  const iter = (currentValue, depth) => {
-    const spacesCount = 4;
-    const replacer = ' ';
-    const indentSize = depth * spacesCount;
-    const cutIndentSize = indentSize - 2;
-    const currentIndent = replacer.repeat(indentSize);
-    const cutCurrentIndent = replacer.repeat(cutIndentSize);
+const defineValue = (value, depth) => {
+  const spacesCount = 4;
+  const indentSize = depth * spacesCount + spacesCount;
+  const bracketIndent = indentSize - spacesCount;
+  const blank = ' ';
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const entries = Object.entries(value);
+  const result = entries.map(([key, val]) => `${blank.repeat(indentSize)}${key}: ${defineValue(val, depth + 1)}`);
+  return ['{', ...result, `${blank.repeat(bracketIndent)}}`].join('\n');
+};
 
-    if (!currentValue.children) {
-      if (currentValue.type === 'added') {
-        return `${cutCurrentIndent}+ ${currentValue.name}: ${makeString(currentValue.value, indentSize)}\n`;
-      } if (currentValue.type === 'unchanged') {
-        return `${cutCurrentIndent}  ${currentValue.name}: ${makeString(currentValue.value, indentSize)}\n`;
-      } if (currentValue.type === 'removed') {
-        return `${cutCurrentIndent}- ${currentValue.name}: ${makeString(currentValue.value, indentSize)}\n`;
-      } if (currentValue.type === 'updated') {
-        return `${cutCurrentIndent}- ${currentValue.name}: ${makeString(currentValue.then, indentSize)}\n${cutCurrentIndent}+ ${currentValue.name}: ${makeString(currentValue.now, indentSize)}\n`;
-      }
+const stylish = (tree) => {
+  const innerFunc = (node, depth) => {
+    const spacesCount = 4;
+    const leftIndent = 2;
+    const indentSize = depth * spacesCount - leftIndent;
+    const blank = ' ';
+    const bracketIndent = indentSize + leftIndent;
+
+    const {
+      key, state, value, oldValue, newValue,
+    } = node;
+
+    if (node.state !== 'nested' && node.state !== 'updated') {
+      return `${blank.repeat(indentSize)}${states[state]}${key}: ${defineValue(value, depth)}`;
     }
-    const nextObjects = currentValue.children.map((child) => iter(child, depth + 1)).join('');
-    return `${currentIndent}${currentValue.name}: {\n${nextObjects}${currentIndent}}\n`;
+    if (node.state === 'updated') {
+      return `${blank.repeat(indentSize)}${states[state][0]}${key}: ${defineValue(oldValue, depth)}\n${blank.repeat(indentSize)}${states[state][1]}${key}: ${defineValue(newValue, depth)}`;
+    }
+    if (node.state === 'nested') {
+      return `${blank.repeat(indentSize)}${states[state]}${key}: {\n${node.value.map((el) => innerFunc(el, depth + 1)).join('\n')}\n${blank.repeat(bracketIndent)}}`;
+    }
+    throw new Error(`Invalid node state - ${state}`);
   };
-  const result = objects.map((object) => iter(object, 1)).join('');
-  return `{\n${result}}`;
+  return `{\n${tree.map((el) => innerFunc(el, 1)).join('\n')}\n}`;
 };
 
 export default stylish;
