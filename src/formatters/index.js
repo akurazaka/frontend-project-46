7d292fbaf63e26c43ce450d4d507fac2556d34f4
Your Name <you@example.com>
2024-09-24 09:49:35 +0600

    test

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,15 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 const getFormat = (tree, formatter = 'stylish') => {
-  if (formatter === 'stylish') {
-    return stylish(tree);
-  }
-  if (formatter === 'plain') {
-    return plain(tree);
-  }
-  if (formatter === 'json') {
-    return JSON.stringify(tree, null, 2);
+  switch (formatter) {
+    case 'stylish':
+      return stylish(tree);
+    case 'plain':
+      return plain(tree);
+    case 'json':
+      return JSON.stringify(tree, null, 2);
+    default:
+      throw new Error(`Unknown formatter: ${formatter}`);
   }
 };
 
