2fb0e56518d17373dfd2e62d25b65c267b0fff93
Your Name <you@example.com>
2024-09-29 20:50:33 +0600

    test

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,13 +1,5 @@
 import _ from 'lodash';
 
-const changeTypes = {
-  added: '+ ',
-  removed: '- ',
-  nested: '  ',
-  updated: ['- ', '+ '],
-  unchanged: '  ',
-};
-
 const formatValue = (data, level) => {
   const indentLevel = 4;
   const currentIndent = level * indentLevel + indentLevel;
@@ -15,14 +7,13 @@ const formatValue = (data, level) => {
   const space = ' ';
 
   if (!_.isObject(data)) {
-    return typeof data === 'string' ? `'${data}'` : data;
+    return data;
   }
 
   const entries = Object.entries(data);
   const formattedEntries = entries.map(
     ([key, val]) => `${space.repeat(currentIndent)}${key}: ${formatValue(val, level + 1)}`,
   );
-
   return ['{', ...formattedEntries, `${space.repeat(closingBracketIndent)}}`].join('\n');
 };
 
@@ -33,45 +24,34 @@ const stylish = (diffTree) => {
     const currentIndent = level * indentLevel - offset;
     const space = ' ';
     const closingBracketIndent = currentIndent + offset;
-
     const {
       key: propertyName,
       type: changeType,
       value: newValue,
       value1: previousValue,
       value2: updatedValue,
-    } = diffNode || {};
-
-    if (!propertyName || !changeType) {
-      throw new Error('Invalid node structure - missing key or type');
-    }
+    } = diffNode;
 
     switch (changeType) {
       case 'added':
+        return `${space.repeat(currentIndent)}+ ${propertyName}: ${formatValue(newValue, level)}`;
+
       case 'removed':
+        return `${space.repeat(currentIndent)}- ${propertyName}: ${formatValue(newValue, level)}`;
+
       case 'unchanged':
-        return `${space.repeat(currentIndent)}${changeTypes[changeType]}${propertyName}: ${formatValue(newValue, level)}`;
+        return `${space.repeat(currentIndent)}  ${propertyName}: ${formatValue(newValue, level)}`;
 
       case 'updated':
-        return `${space.repeat(currentIndent)}${changeTypes[changeType][0]}${propertyName}: ${formatValue(previousValue, level)}\n` +
-               `${space.repeat(currentIndent)}${changeTypes[changeType][1]}${propertyName}: ${formatValue(updatedValue, level)}`;
+        return `${space.repeat(currentIndent)}- ${propertyName}: ${formatValue(previousValue, level)}\n${space.repeat(currentIndent)}+ ${propertyName}: ${formatValue(updatedValue, level)}`;
 
       case 'nested':
-        if (!Array.isArray(newValue)) {
-          throw new Error(`Expected an array for nested property '${propertyName}', but got ${typeof newValue}`);
-        }
-        return `${space.repeat(currentIndent)}${changeTypes[changeType]}${propertyName}: {\n` +
-               `${newValue.map((childNode) => processNode(childNode, level + 1)).join('\n')}\n` +
-               `${space.repeat(closingBracketIndent)}}`;
+        return `${space.repeat(currentIndent)}  ${propertyName}: {\n${diffNode.value.map((childNode) => processNode(childNode, level + 1)).join('\n')}\n${space.repeat(closingBracketIndent)}}`;
 
       default:
         throw new Error(`Invalid node type - ${changeType}`);
     }
   };
-
-  if (!Array.isArray(diffTree)) {
-    throw new Error('Invalid diffTree structure, expected an array');
-  }
 
   return `{\n${diffTree.map((childNode) => processNode(childNode, 1)).join('\n')}\n}`;
 };
